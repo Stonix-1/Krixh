4eb43b1f9b73d5f8537eaf27b563904e7c4c167f
Stonix-1 <stonixowak704@gmail.com>
2024-12-14 19:41:59 +0545

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -34,7 +34,7 @@ module.exports = {
       const categories = {};
       let msg = "";
 
-      msg += `╔═══════════╗\n ★𝙃𝙚𝙮 𝙞𝙖𝙢 Ryuken 𝙜𝙤𝙖𝙩𝙗𝙤𝙩.𝙈𝙮 𝙥𝙧𝙚𝙛𝙞𝙭 , 𝙢𝙮 𝙤𝙬𝙣𝙚𝙧 DENISH THARU. fb- fb.me/100086747072197&mibextid=kFxxJD 💐\n╚═══════════╝`; // replace with your name 
+      msg += `╔═══════════╗\n ★𝙃𝙚𝙮 𝙞𝙖𝙢 Krish 𝙜𝙤𝙖𝙩𝙗𝙤𝙩.𝙈𝙮 𝙥𝙧𝙚𝙛𝙞𝙭 + 𝙢𝙮 𝙤𝙬𝙣𝙚𝙧 KRISH ERA. fb- fb.me/100086734771592&mibextid=kFxxJD 💐\n╚═══════════╝`; // replace with your name 
 
       for (const [name, value] of commands) {
         if (value.config.role > 1 && role < value.config.role) continue;
@@ -62,7 +62,7 @@ module.exports = {
       const totalCommands = commands.size;
       msg += `\n𝗖𝘂𝗿𝗿𝗲𝗻𝘁𝗹𝘆, 𝘁𝗵𝗲 𝗯𝗼𝘁 𝗵𝗮𝘀 ${totalCommands} 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀 𝘁𝗵𝗮𝘁 𝗰𝗮𝗻 𝗯𝗲 𝘂𝘀𝗲𝗱\n`;
       msg += `𝗧𝘆𝗽𝗲 ${prefix} 𝗵𝗲𝗹𝗽 𝗰𝗺𝗱𝗡𝗮𝗺𝗲 𝘁𝗼 𝘃𝗶𝗲𝘄 𝘁𝗵𝗲 𝗱𝗲𝘁𝗮𝗶𝗹𝘀 𝗼𝗳 𝘁𝗵𝗮𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱\n`
-      msg += `🐐 | Ryuk4zi`; // its not decoy so change it if you want 
+      msg += `🐐 | Krish`; // its not decoy so change it if you want 
 
       const helpListImages = [
         "https://i.ibb.co/nm5kLpN/image.gif"
